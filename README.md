421f4240e793a14e96befe215efffc91234d2be3
Sarah Gooding <keepingcozy@gmail.com>
2014-03-04 23:47:44 -0600

    updated theme name and slugs

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-Intrepid
+Ex Astris
 ========
 
-Intrepid is a WordPress child theme for the excellent <a href="http://themehybrid.com/themes/stargazer">Stargazer</a> theme from <a href="http://themehybrid.com/">Theme Hybrid</a>. This bold theme was designed for bloggers. Make it your own with a customer header and custom background. Intrepid supports all of the custom layouts found in the Stargazer parent theme, including  1 Column Wide, 1 Column Narrow, 2 Columns: Content / Sidebar, and 2 Columns: Sidebar / Content.
+Ex Astris is a WordPress child theme for the excellent <a href="http://themehybrid.com/themes/stargazer">Stargazer</a> theme from <a href="http://themehybrid.com/">Theme Hybrid</a>. This bold theme was designed for bloggers. Make it your own with a customer header and custom background. Ex Astris supports all of the custom layouts found in the Stargazer parent theme, including  1 Column Wide, 1 Column Narrow, 2 Columns: Content / Sidebar, and 2 Columns: Sidebar / Content.
 
-A live demo is available at: <a href="http://untame.net/intrepid/">untame.net/intrepid</a>
+A live demo is available at: <a href="http://untame.net/ex-astris/">untame.net/ex-astris</a>
 
-<strong>Credits:</strong> 
+<strong>Credits:</strong>
 
 Font - Copse by Dan Rhatigan, available from <a href="https://www.google.com/fonts">Google Fonts</a> - <a href="http://scripts.sil.org/OFL">SIL Open Font License, 1.1</a>
 
--- functions.php
+++ functions.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * @package    Intrepid
+ * @package    Ex Astris
  * @version    1.0.0
- * @author     Sarah Gooding <sarah@untame.net>
+ * @author     Sarah Gooding <keepingcozy@gmail.com>
  * @copyright  Copyright (c) 2014, Sarah Gooding
- * @link       http://themehybrid.com/themes/intrepid
+ * @link       http://themehybrid.com/themes/ex-astris
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 /* Add the child theme setup function to the 'after_setup_theme' hook. */
-add_action( 'after_setup_theme', 'intrepid_theme_setup' );
+add_action( 'after_setup_theme', 'ex_astris_theme_setup' );
 
 /**
  * @since  1.0.0
  * @access public
  * @return void
  */
-function intrepid_theme_setup() {
+function ex_astris_theme_setup() {
 
 	/*
-	 * Add a custom background to overwrite the defaults.  Remove this section if you want to use 
+	 * Add a custom background to overwrite the defaults.  Remove this section if you want to use
 	 * the parent theme defaults instead.
 	 *
 	 * @link http://codex.wordpress.org/Custom_Backgrounds
@@ -33,24 +33,24 @@ function intrepid_theme_setup() {
 			'default-position'   => 'center',
 			'default-position-x' => 'center',
 			'default-attachment' => 'fixed',
-  
+
 		)
 	);
 
 
     /* Add suport for custom header but show none by default. */
-    add_theme_support( 'custom-header', array( 'default-image' => '' ) );  
+    add_theme_support( 'custom-header', array( 'default-image' => '' ) );
 
 	/* Filter to add custom default backgrounds (supported by the framework). */
-	add_filter( 'hybrid_default_backgrounds', 'intrepid_default_backgrounds' );
+	add_filter( 'hybrid_default_backgrounds', 'ex_astris_default_backgrounds' );
 
 	/* Add a custom default color for the "primary" color option. */
-	add_filter( 'theme_mod_color_primary', 'intrepid_color_primary' );
-  
-    /* Add custom stylesheets. */  
-    add_action( 'wp_enqueue_scripts', 'intrepid_enqueue_styles' );
-  
-  
+	add_filter( 'theme_mod_color_primary', 'ex_astris_color_primary' );
+
+    /* Add custom stylesheets. */
+    add_action( 'wp_enqueue_scripts', 'ex_astris_enqueue_styles' );
+
+
 }
 
 /**
@@ -59,14 +59,14 @@ function intrepid_theme_setup() {
  * @param  array  $backgrounds
  * @return array
  */
-function intrepid_default_backgrounds( $backgrounds ) {
+function ex_astris_default_backgrounds( $backgrounds ) {
 
 	$new_backgrounds = array(
 		'green' => array(
 			'url'           => '%2$s/images/backgrounds/green.jpg',
 			'thumbnail_url' => '%2$s/images/backgrounds/green-thumb.jpg',
 		),
-		
+
 	);
 
 	return array_merge( $new_backgrounds, $backgrounds );
@@ -78,20 +78,20 @@ function intrepid_default_backgrounds( $backgrounds ) {
  * @param  string  $hex
  * @return string
  */
-function intrepid_color_primary( $hex ) {
+function ex_astris_color_primary( $hex ) {
 	return $hex ? $hex : '111111';
 }
 
 
 /**
 * Loads custom stylesheets for the theme.
-* 
+*
 * @since  1.0.0
 * @access public
 * @return void
 */
 
-function intrepid_enqueue_styles() {
+function ex_astris_enqueue_styles() {
             wp_register_style('googleFonts', 'http://fonts.googleapis.com/css?family=Copse');
             wp_enqueue_style( 'googleFonts');
         }
